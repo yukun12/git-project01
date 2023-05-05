65b41ab46e492583bcb9631a22830c507e4b08d2
yk <yk@163.com>
2023-05-05 18:15:30 +0800

    yk second commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,5 +5,6 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args){
         System.out.println("Hello Git");
+        System.out.println("Hello Git yk second commit");
     }
 }

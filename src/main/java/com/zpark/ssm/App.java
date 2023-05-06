44ee7ce84081024140365c1e08dabf0852ee6a99
yk <yk@163.com>
2023-05-06 09:06:08 +0800

    yk hotfix first commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -6,5 +6,6 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello Git");
         System.out.println("Hello Git yk second commit");
+        System.out.println("Hello Git! yk hotfix first commit!");
     }
 }
